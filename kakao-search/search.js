b510d23ff11aaddf4558d297aa4fb50178f68d6b
seung <ksh990916@gmail.com>
2023-01-26 15:26:53 +0900

    kakao search

--- kakao-search/search.js
+++ kakao-search/search.js
@@ -1,24 +1,55 @@
 // promise 이행을 하거나 거절을 하거나
 const searchTxt = document.querySelector(".search-txt");
 const btnSearch = document.querySelector(".btn-search");
-const ul = document.querySelector(".list");
-const myFetch = fetch("http://dapi.kakao.com/v2/search/image?query=김태희", {
-  headers: { Authorization: "KakaoAK 7025b57e2c141dff70e233b7612ec4ee" },
+const thumbList = document.querySelector(".list");
+const recentSearchWord = document.querySelector(".recent-search-word");
+//let recentSearchWordArray = [];
+const recentSearchWordArray =
+  JSON.parse(localStorage.getItem("recentSearchWord")) ?? [];
+
+recentSearchWordArray.forEach(function (item, idx) {
+  recentSearchWord.innerHTML += `<li>${item}</li>`;
 });
 
-myFetch
-  .then(function (response) {
-    const json = response.json();
+searchTxt.addEventListener("keyup", function (e) {
+  const txt = searchTxt.value;
 
-    json.then(function (result) {
-      //console.log(result.documents[0]);
-      const list = result.documents;
+  if (e.keyCode === 13) {
+    searchImg(txt);
 
-      for (let i = 0; i < list.length; i++) {
-        ul.innerHTML += `<li><img src="${list[i].thumbnail_url}"></li>`;
-      }
+    if (!recentSearchWordArray.includes(txt)) {
+      recentSearchWordArray.push(txt);
+      recentSearchWord.innerHTML += `<li>${txt}</li>`;
+
+      localStorage.setItem(
+        "recentSearchWord",
+        JSON.stringify(recentSearchWordArray)
+      );
+    }
+  }
+});
+
+function searchImg(searchTxt) {
+  thumbList.innerHTML = "";
+
+  const myFetch = fetch(
+    `http://dapi.kakao.com/v2/search/image?query=${searchTxt}`,
+    {
+      headers: { Authorization: "KakaoAK 7025b57e2c141dff70e233b7612ec4ee" },
+    }
+  );
+
+  myFetch
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      result.documents.forEach(function (item, idx) {
+        console.log(item.thumbnail_url);
+        thumbList.innerHTML += `<li><a href="${item.image_url}" data-fancybox="gallery"><img src="${item.thumbnail_url}"></a></li>`;
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+}
